4f50d4bf59769fa0a0b8a557633efe248d80cdfe
pranay <pranayanand61@gmail.com>
2023-05-17 06:31:17 +0530

    initialize and declare the variable name with customername and print it on console

---
+++ src/main/java/com/niit/jdp/WatAPizza.java
@@ -0,0 +1,8 @@
+package com.niit.jdp;
+
+public class WatAPizza {
+    public static void main(String[] args) {
+        String customerName = "Garry";
+        System.out.println("customerName" + " = " + customerName);
+    }
+}
